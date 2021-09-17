0db0592cb11ddfff2eb6d872fe1a20f0b6244290
satoshi.473901.1205@gmail.com <ec2-user@ip-172-31-90-77.ec2.internal>
2021-09-17 09:24:56 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '4946df756aba65be932e1c41854ee9826a87d7af55a0a68f62cf314a4624fd9702eeca5d235a7fc8ab87b7f1f269bed00d4ba29a48e6e3a1d6865d58e2c59536'
